42e5d260b3e79a00c03d15a7a549d034e1e3211e
iskandarriski <81105032+iskandarriski@users.noreply.github.com>
2021-06-29 09:58:22 +0700

    Add files via upload

---
+++ BinaryTreeApp.java
@@ -0,0 +1,40 @@
+
+package soall3;
+
+import com.sun.source.tree.BinaryTree;
+
+public class BinaryTreeApp {
+   public static void main(String[] args){
+        BinaryTree tree = new BinaryTree() {};
+        
+        TreeNode node;
+        
+        node = new TreeNode(50);
+        tree.insert(node);
+        
+        node = new TreeNode(30);
+        tree.insert(node);
+    
+        node = new TreeNode(20);
+        tree.insert(node);
+    
+        node = new TreeNode(40);
+        tree.insert(node);
+    
+        node = new TreeNode(70);
+        tree.insert(node);
+    
+        System.out.print("\nTraversal dengan InOrder :");
+        tree.inOrder();
+        
+        System.out.print("\nTraversal dengan preorder :");
+        tree.preOrder();
+        
+        System.out.print("\nTraversal dengan Postorder :");
+        tree.postOrder();
+        
+        
+        System.out.println();
+    }
+}  
+
